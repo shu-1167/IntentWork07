0e0df798f764a2939776acc54a06f793cc583ea8
shu-1167 <shu_11671@yahoo.co.jp>
2020-08-13 19:37:42 +0900

    [Update] Processing when date header is none and multipart processing

---
+++ app/src/main/java/in3a_2180289/intentwork07/Mail.kt
@@ -0,0 +1,9 @@
+package in3a_2180289.intentwork07
+
+class Mail {
+    val thread = Thread(Runnable {
+        kotlin.run {
+
+        }
+    })
+}
--- app/src/main/java/in3a_2180289/intentwork07/MainActivity.kt
+++ app/src/main/java/in3a_2180289/intentwork07/MainActivity.kt
@@ -5,19 +5,23 @@ import android.os.StrictMode
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
-import java.text.SimpleDateFormat
+import java.text.DateFormat
 import java.util.*
 import javax.mail.*
+import javax.mail.internet.MimeMultipart
 import javax.mail.internet.MimeUtility
 
 
 class MainActivity : AppCompatActivity() {
 
     private val host = "example.com"
+
     // mail address
-    private val user = "hogehoge@example.com"
+    private val user = "hogehoge"
+
     // IMAPS port
     private val port = 993
+
     // password
     private val pass = "xxxxxx"
 
@@ -30,86 +34,86 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-
     private fun mail() {
-        //        try {
 
         val policy = StrictMode.ThreadPolicy.Builder().permitNetwork().build()
         StrictMode.setThreadPolicy(policy)
-// http://connector.sourceforge.net/doc-files/Properties.html
-        val props: Properties = System.getProperties()
+        // http://connector.sourceforge.net/doc-files/Properties.html
+        val props = Properties()
+        // props.setProperty("mail.debug", "true")
         // セッション
         val session: Session = Session.getInstance(props, null)
         // IMAP で SSL を使用する
         val imap4: Store = session.getStore("imaps")
 
-// 接続
+        // 接続
         imap4.connect(host, port, user, pass)
-        // println(imap4.urlName.toString())
         Log.d(this.localClassName, imap4.urlName.toString())
 
-// https://stackoverflow.com/questions/11435947/how-do-i-uniquely-identify-a-java-mail-message-using-imap
+        // https://stackoverflow.com/questions/11435947/how-do-i-uniquely-identify-a-java-mail-message-using-imap
         val folder = imap4.getFolder("INBOX")
         val uf = folder as UIDFolder
         folder.open(Folder.READ_ONLY)
 
-// 全てのメッセージ
+        // 全てのメッセージ
         val totalMessages = folder.messageCount
-        // println("Total messages = $totalMessages")
         Log.d(this.localClassName, "Total messages = $totalMessages")
 
-// 新しいメッセージ
+        // 新しいメッセージ
         val newMessages = folder.newMessageCount
-        // println("New messages = $newMessages")
         Log.d(this.localClassName, "New messages = $newMessages")
 
-// メッセージの一覧を取得
+        // メッセージの一覧を取得
         val msgs: Array<Message> = folder.messages
 
-// メッセージがコピー・移動されていると日付順とは限らない
+        // メッセージがコピー・移動されていると日付順とは限らない
         for (i in msgs.indices.reversed()) {
 
-// UID を取得
+            // UID を取得
             val messageId = uf.getUID(msgs[i])
             // println("UID = $messageId")
             Log.d(this.localClassName, "UID = $messageId")
 
-// From
-            val address: Array<Address> = msgs[i].getFrom()
-            var addressText = ""
-            if (address != null) {
-// address[0].toString() のままでは正しく表示されない
-                addressText = MimeUtility.decodeText(address[0].toString())
-            }
+            // From
+            val address: Array<Address> = msgs[i].from
+            // address[0].toString() のままでは正しく表示されない
+            val addressText = MimeUtility.decodeText(address[0].toString())
             // println("Address = $addressText")
             Log.d(this.localClassName, "Address = $addressText")
 
-// Subject
+            // Subject
             val subjectText: String = MimeUtility.decodeText(msgs[i].getSubject())
             // println("Subject = $subjectText")
             Log.d(this.localClassName, "Subject = $subjectText")
 
-// 受信日時
-            val date: Date = msgs[i].getSentDate()
-            val sdf = SimpleDateFormat("yyyy/MM/dd HH:mm:ss")
-            val dateText: String = sdf.format(date)
+            val date: Date = if (msgs[i].sentDate != null) {
+                // Dateヘッダ(送信日時)がある場合、そちらを使用
+                msgs[i].sentDate
+            } else {
+                // Dateヘッダがない場合、受信日時を使用
+                msgs[i].receivedDate
+            }
+            // val sdf = SimpleDateFormat("yyyy/MM/dd HH:mm:ss")
+            val dateText = DateFormat.getDateTimeInstance().format(date)
             // println("Date = $dateText")
             Log.d(this.localClassName, "Date = $dateText")
 
-// 本文(  UID より取得 )
-            val msg: Message = uf.getMessageByUID(messageId)
-            val part: Part = msg
+            // 本文(  UID より取得 )
+            // val msg: Message = uf.getMessageByUID(messageId)
+            val part: Part = msgs[i]
             var bodyText = ""
             if (part.isMimeType("text/plain")) {
-                bodyText = part.getContent().toString()
-//                } else if (part.isMimeType("multipart/*")) {
-//                    val mp = part.getContent()
-//                    // マルチパートの先頭
-//                    bodyText = mp.getBodyPart(0).content.toString()
-//                    if (mp.getBodyPart(1).isMimeType("text/html")) {
-//                        val htmlText = mp.getBodyPart(1).content.toString()
-//                        println("HtmlText = $htmlText")
-//                    }
+                bodyText = part.content.toString()
+            } else if (part.isMimeType("multipart/*")) {
+                // マルチパートの先頭
+                val mp = part.content as MimeMultipart
+                bodyText = mp.getBodyPart(0).content.toString()
+                for (j in 1..mp.count.dec()) {
+                    if (mp.getBodyPart(j).isMimeType("text/html")) {
+                        val htmlText = mp.getBodyPart(j).content.toString()
+                        println("HtmlText = $htmlText")
+                    }
+                }
 //                    var filename: String = (mp.getBodyPart(1) as Part).getFileName()
 //                    // ファイル名があったら保存
 //                    if (filename != null) {
@@ -130,14 +134,7 @@ class MainActivity : AppCompatActivity() {
 //                    }
             Log.d(this.localClassName, "Body = $bodyText")
         }
-//                println("Body = $bodyText")
-//            }
         folder.close()
         imap4.close()
-//        } catch (ex: Exception) {
-// handle any errors
-        // println("Exception: " + ex.message)
-//            Log.d(this.localClassName, "Exception: " + ex.message)
-//        }
     }
 }
