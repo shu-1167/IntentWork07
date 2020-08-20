5394a030ee7b6d886800883df3b9738776049e1b
shu-1167 <shu_11671@yahoo.co.jp>
2020-08-21 00:48:35 +0900

    [Upgrade] Use sqlite database to store account information

---
+++ app/src/main/java/in3a_2180289/intentwork07/MailDBHelper.kt
@@ -0,0 +1,28 @@
+/**
+ * スマートフォン・アプリ演習Ⅰ
+ * 第6章オリジナル作品制作（その２）
+ * データベース定義用クラス
+ *
+ * 作成日：2020/08/05
+ * @author shu-1167
+ */
+
+package in3a_2180289.intentwork07
+
+import android.content.Context
+import android.database.sqlite.SQLiteDatabase
+import android.database.sqlite.SQLiteOpenHelper
+
+class MailDBHelper(
+    context: Context,
+    databaseName: String,
+    factory: SQLiteDatabase.CursorFactory?,
+    version: Int
+) : SQLiteOpenHelper(context, databaseName, factory, version) {
+    override fun onCreate(database: SQLiteDatabase?) {
+        database?.execSQL("CREATE TABLE IF NOT EXISTS users (user_id INTEGER PRIMARY KEY AUTOINCREMENT, email TEXT, username TEXT, password TEXT)")
+        database?.execSQL("CREATE TABLE IF NOT EXISTS mails (user_id INTEGER, uid INTEGER, subject TEXT, sender TEXT, date TEXT, body BLOB, PRIMARY KEY(user_id, uid))")
+    }
+
+    override fun onUpgrade(database: SQLiteDatabase?, oldVersion: Int, newVersion: Int) {}
+}
--- app/src/main/java/in3a_2180289/intentwork07/MainActivity.kt
+++ app/src/main/java/in3a_2180289/intentwork07/MainActivity.kt
@@ -7,15 +7,19 @@
  */
 package in3a_2180289.intentwork07
 
+import android.content.ContentValues
 import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
+import android.util.Base64
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.RecyclerView
 import androidx.swiperefreshlayout.widget.SwipeRefreshLayout
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
+import java.nio.charset.Charset
 
 
 class MainActivity : AppCompatActivity() {
@@ -28,18 +32,12 @@ class MainActivity : AppCompatActivity() {
     }
 
     private val requestCode = 1
-
-    // hostname
     private var host = String()
-
-    // username
     private var user = String()
-
-    // IMAPS port
     private var port = 993
-
-    // password
     private var pass = String()
+    private val dbName = "mail"
+    private val dbVersion = 1
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -53,10 +51,41 @@ class MainActivity : AppCompatActivity() {
         mSwipeRefreshLayout.setOnRefreshListener(mOnRefreshListener)
 
         // アカウント追加アクティビティ
-        val intent = Intent(this, AccountAddActivity::class.java)
-        startActivityForResult(intent, requestCode)
+        val sharedPref = getSharedPreferences("mail", MODE_PRIVATE)
+        val defAccount = sharedPref.getInt("defaultAccount", -1)
+        if (defAccount == -1) {
+            // アカウントがない場合追加する
+            val intent = Intent(this, AccountAddActivity::class.java)
+            startActivityForResult(intent, requestCode)
+        } else {
+            // デフォルトアカウント取得
+            val dbHelper = MailDBHelper(this, dbName, null, dbVersion)
+            val database = dbHelper.readableDatabase
+
+            val cursor = database.query(
+                "users",
+                arrayOf("email", "username", "password"),
+                "user_id = ?",
+                arrayOf(defAccount.toString()),
+                null,
+                null,
+                null
+            )
+            cursor.moveToFirst()
+            if (cursor.count == 1) {
+                val addr = cursor.getString(0)
+                host = addr.split('@')[1]
+                user = cursor.getString(1)
+                // 入ってるパスワードはbase64なのでデコード
+                val b64Password = cursor.getString(2)
+                pass = Base64.decode(b64Password.toByteArray(), Base64.DEFAULT)
+                    .toString(Charset.defaultCharset())
+            }
+            cursor.close()
+        }
     }
 
+    // 他のアクティビティから帰ってきたら呼ばれる
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
 
@@ -66,9 +95,36 @@ class MainActivity : AppCompatActivity() {
             host = addr.split('@')[1]
             user = addr.split('@')[0]
             pass = data.getStringExtra("password")!!
+            // データべースへ追加
+            insertUser(addr, user, pass)
+
+            // 初期アカウントの場合、デフォルトアカウントに指定
+            val sharedPref = getSharedPreferences("mail", MODE_PRIVATE)
+            if (sharedPref.getInt("defaultAccount", -1) == -1) {
+                val dbHelper = MailDBHelper(this, dbName, null, dbVersion)
+                val database = dbHelper.readableDatabase
+
+                // 念のため追加したアカウントのuser_idを取得
+                val cursor =
+                    database.query("users", arrayOf("MAX(user_id)"), null, null, null, null, null)
+                cursor.moveToFirst()
+                val userId = if (cursor.count == 1) {
+                    cursor.getInt(0)
+                } else {
+                    // データがない
+                    -1
+                }
+                cursor.close()
+
+                // デフォルトアカウントをセット
+                val editor = sharedPref.edit()
+                editor.putInt("defaultAccount", userId)
+                editor.apply()
+            }
         }
     }
 
+    // 引っ張って更新されたら呼ばれる
     private val mOnRefreshListener = SwipeRefreshLayout.OnRefreshListener {
         val coroutineScope = CoroutineScope(Dispatchers.IO)
         // メールクラス生成
@@ -76,6 +132,24 @@ class MainActivity : AppCompatActivity() {
         coroutineScope.launch {
             // 受信処理
             mail.receive()
+        }
+    }
+
+    private fun insertUser(email: String, username: String, password: String) {
+        val dbHelper = MailDBHelper(this, dbName, null, dbVersion)
+        val database = dbHelper.writableDatabase
+
+        val values = ContentValues()
+        values.put("email", email)
+        values.put("username", username)
+        // パスワードはbase64エンコード
+        val b64Password = Base64.encodeToString(password.toByteArray(), Base64.DEFAULT)
+        values.put("password", b64Password)
+
+        try {
+            database.insertOrThrow("users", null, values)
+        } catch (ex: Exception) {
+            Toast.makeText(this, getString(R.string.failed_add_account), Toast.LENGTH_LONG).show()
         }
     }
 }
