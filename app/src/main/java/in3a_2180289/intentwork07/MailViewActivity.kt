5eebce7eec8e37bc322273d0c3cbf3f0c4fca6ae
shu-1167 <shu_11671@yahoo.co.jp>
2020-08-25 19:58:04 +0900

    [Update] Change display of email body from TextView to WebView
    To be able to display HTML mail

--- app/src/main/java/in3a_2180289/intentwork07/MailViewActivity.kt
+++ app/src/main/java/in3a_2180289/intentwork07/MailViewActivity.kt
@@ -1,8 +1,7 @@
 package in3a_2180289.intentwork07
 
 import android.os.Bundle
-import android.text.method.ScrollingMovementMethod
-import android.widget.TextView
+import android.webkit.WebView
 import androidx.appcompat.app.AppCompatActivity
 
 class MailViewActivity : AppCompatActivity() {
@@ -10,12 +9,12 @@ class MailViewActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_mail_view)
 
-        val bodyText: TextView = findViewById(R.id.textView)
-        bodyText.movementMethod = ScrollingMovementMethod()
-        // テキストを選択(コピー)できるようにする
-        bodyText.setTextIsSelectable(true)
-        // 受け取ったメール本文を表示
-        bodyText.text = intent.getStringExtra("body")
+        // タイトルのセット
         title = intent.getStringExtra("subject")
+        // 本文の取得
+        val body = intent.getStringExtra("body")
+        val webView: WebView = findViewById(R.id.webView)!!
+        // 受け取ったメール本文を表示
+        webView.loadData(body, "text/plain", "utf-8")
     }
 }
